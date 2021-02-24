de3673ef73468f928f4d9b6a1f21eb87ad14460b
elinordin <elin.evelina@live.se>
2021-02-24 14:51:27 +0100

    adding buttun to navigate between screens

--- src/containers/trainerTrainee.js
+++ src/containers/trainerTrainee.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function trainerTrainee({navigation}) {
   return (
     <View style={styles.container}>
       <Text>This is the trainer / trainee screen</Text>
+      <Button title="Trainee" onPress={() =>
+        navigation.navigate('Trainer Picker')
+      }/>
       <StatusBar style="auto" />
     </View>
   );
