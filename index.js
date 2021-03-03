f192f9a5beab1a033c357b1ccbb92182659d4fe2
Rafe Nasir <rafenasir@gmail.com>
2021-03-03 01:16:42 +0100

    Comment added to check the git config

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ import App from './src/App';
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+//Comment added to check git
