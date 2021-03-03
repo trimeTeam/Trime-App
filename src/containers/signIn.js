14e6bae9749f1c1922bedeb34a88c74117ff28cf
Rafe Nasir <rafenasir@gmail.com>
2021-03-03 19:34:37 +0100

    updated the basic structure in container folder

--- src/containers/signIn.js
+++ src/containers/signIn.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import FontAwsome5 from 'react-native-vector-icons';
 
 export default function signIn({navigation}) {
   return (
@@ -11,6 +12,23 @@ export default function signIn({navigation}) {
       <TextInput style={styles.textInput} />
       <Text style={styles.inputHeading}>Password</Text>
       <TextInput style={styles.textInput} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Create Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.buttonGuest}>
+        <Text style={styles.buttonText}>Create Account</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.inputHeading}>OR</Text>
+      <TouchableOpacity style={styles.fbButton}>
+        <Text style={styles.buttonText}>Sign in with Facebook</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.googleButton}>
+        <Text style={styles.buttonText}>Sign in with Google</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.twitterButton}>
+        <Text style={styles.buttonText}>Sign in with Twitter</Text>
+      </TouchableOpacity>
 
 
     </View>
@@ -27,24 +45,68 @@ const styles = StyleSheet.create({
   introText: {
     fontSize: 14,
     marginTop: 0,
-    margin: 'auto'
+    // margin: 'auto'
+    marginBottom: 10,
+    marginTop: 10
       
   },
 
   textInput: {
     height: 40, 
-    width: 580,
-    borderColor: 'gray', 
+    width: 330,
+    borderBottomColor: 'gray', 
     borderWidth: 1
   },
   inputHeading: {
-    hfontSize: 14,
+    fontSize: 14,
     marginTop: 0,
 
   },
-  header: {
-    fontSize: 28,
-    fontWeight: 'bold', 
-    marginBottom: 20
+  button: {
+    marginTop: 10,
+    width: 330,
+    height: 50,
+    backgroundColor: '#FDB339',
+    borderRadius: 50,
+    justifyContent: 'center'
   },
+
+  buttonText: {
+    fontSize: 18,
+    color: 'black',
+    margin:'auto',
+  },
+
+  buttonGuest: {
+    marginTop: 10,
+    width: 330,
+    height: 50,
+    backgroundColor: '#F6F6F6',
+    borderRadius: 50,
+    justifyContent: 'center'
+  },
+
+  fbButton:{
+    marginTop: 10,
+    width: 330,
+    height: 50,
+    backgroundColor: '#3B5998',
+    // borderRadius: 50,
+    justifyContent: 'center'
+  },
+
+  googleButton:{
+    marginTop: 10,
+    width: 330,
+    height: 50,
+    backgroundColor: '#DD4B39',
+    justifyContent: 'center'
+  },
+  twitterButton: {
+    marginTop: 10,
+    width: 330,
+    height: 50,
+    backgroundColor: '#55ACEE',
+    justifyContent: 'center'
+  }
 });
